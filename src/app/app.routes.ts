e05d911a03e53e04a24866c539bee993eb6b5e45
Nellehmdlll <sdgrahima@gmail.com>
2025-06-16 09:45:32 +0000

    'firs_commit'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UsersComponent } from './pages/users/users.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'users', component: UsersComponent },
+      { path: 'settings', component: SettingsComponent }
+    ]
+  }
+];
---
+++ src/app/layout/layout/layout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { HeaderComponent } from '../header/header.component';
+import { RouterModule } from '@angular/router'; // ✅ Importer RouterModule ici
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [FooterComponent, SidebarComponent, HeaderComponent,RouterModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+})
+export class LayoutComponent {
+
+}
